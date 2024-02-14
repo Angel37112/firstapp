2c33b93f4b2cece2a30072120085a5f70d63e3e6
Angelogama21 <ang21gm33@gmail,com>
2024-02-14 07:30:15 -0600

    Second Commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import styles from "@/styles/index.module.css";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.container}>
         <nav className={styles.navbar}>
           <a href="#">Home</a>
           <a href="#">Acerca de...</a>
         </nav>
+      </div>
+      <div className={styles.content}>
+        <h2>Hola, mi nombre es Ángel García Mendoza del grupo TI03SM-22</h2>
       </div>
     </main>
   );
